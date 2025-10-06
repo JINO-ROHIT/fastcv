42a6037f214cf518fac4b18b04828b0ad9691e34
JINO-ROHIT <find.jinorohit@gmail.com>
2025-10-06 10:23:10 +0530

    sobel kernel

--- kernels/sobel.cu
+++ kernels/sobel.cu
@@ -26,7 +26,7 @@ __global__ void sobelKernel(unsigned char* Pin, unsigned char* Pout, int width, 
     }
 
     // sobel kernels
-    const int Kx[3][3] = { { -1, 0, 1 }, { -2, 0, 2 }, { -1, 0, 1 } };
+    const int Kx[3][3] = { { -1, 0, 1 }, { -2, 0, 2 }, { -1, 0, 1 } }; 
     const int Ky[3][3] = { { -1, -2, -1 }, { 0, 0, 0 }, { 1, 2, 1 } };
 
     int gx = 0;
@@ -41,10 +41,10 @@ __global__ void sobelKernel(unsigned char* Pin, unsigned char* Pout, int width, 
             unsigned char I_in = 0;
             if (nr >= 0 && nr < height && nc >= 0 && nc < width) {
                 I_in = Pin[nr * width + nc];
+                gx += I_in * Kx[i + 1][j + 1];
+                gy += I_in * Ky[i + 1][j + 1];
             }
 
-            gx += I_in * Kx[i + 1][j + 1];
-            gy += I_in * Ky[i + 1][j + 1];
         }
     }
     
@@ -63,13 +63,15 @@ torch::Tensor sobel(torch::Tensor img){
     TORCH_CHECK(img.device().type() == torch::kCUDA);
     TORCH_CHECK(img.dtype() == torch::kByte);
 
+    img = img.contiguous();
+
     const auto height = img.size(0);
     const auto width = img.size(1);
 
     dim3 dimBlock = getOptimalBlockDim(width, height);
     dim3 dimGrid(cdiv(width, dimBlock.x), cdiv(height, dimBlock.y));
 
-    auto result = torch::empty({height, width, 1}, 
+    auto result = torch::empty({height, width}, 
                                torch::TensorOptions()
                                    .dtype(torch::kByte)
                                    .device(img.device()));
