5c08864262fba90963797c9d26c7b4f20fd8e676
JINO-ROHIT <find.jinorohit@gmail.com>
2025-10-07 22:13:29 +0530

    dilation kernels

--- kernels/module.cpp
+++ kernels/module.cpp
@@ -4,9 +4,11 @@
 torch::Tensor rgb_to_gray(torch::Tensor img);
 torch::Tensor gaussian_blur(torch::Tensor img, int blurSize);
 torch::Tensor sobel(torch::Tensor img);
+torch::Tensor dilation(torch::Tensor img, int filterSize);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m){
     m.def("rgb2gray", &rgb_to_gray, "rgb to grayscale kernel");
     m.def("blur", &gaussian_blur, "gaussian blur kernel");
     m.def("sobel", &sobel, "sobel filter kernel");
+    m.def("dilate", &dilation, "dilation kernel");
 }
